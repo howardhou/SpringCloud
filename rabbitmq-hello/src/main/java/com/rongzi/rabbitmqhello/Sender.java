f92bcf89cd64a60ff71c3d1e60196cd67c05f733
houdongdong <houdongdong@rongzi.com>
2018-08-24 15:08:26 +0800

    使用 RabbitMQ 依赖包，测试发送和接收消息

---
+++ rabbitmq-hello/src/main/java/com/rongzi/rabbitmqhello/Sender.java
@@ -0,0 +1,26 @@
+package com.rongzi.rabbitmqhello;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+
+@Component
+public class Sender {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send(){
+        String context = "hello " + new Date();
+        System.out.println("\n");
+        System.out.println("Sender : " + context);
+
+        // 发送到 Hello 队列中
+        this.rabbitTemplate.convertAndSend("hello", context);
+    }
+
+
+}
