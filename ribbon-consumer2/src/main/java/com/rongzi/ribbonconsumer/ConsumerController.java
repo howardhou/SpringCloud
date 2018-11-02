dc51a3fe948b635efa588bdf70445a1223007b65
houdongdong <houdongdong@rongzi.com>
2018-11-02 16:38:19 +0800

    测试RxJava

--- ribbon-consumer2/src/main/java/com/rongzi/ribbonconsumer/ConsumerController.java
+++ ribbon-consumer2/src/main/java/com/rongzi/ribbonconsumer/ConsumerController.java
@@ -4,6 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+
+import java.util.ArrayList;
+
 
 @RestController
 public class ConsumerController {
@@ -16,7 +22,39 @@ public class ConsumerController {
 
     @RequestMapping(value = "/ribbon-consumer")
     public String helloConsumer(){
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+//        Observable.from(list).subscribe(new Subscriber<Integer>() {
+//            @Override
+//            public void onCompleted() {
+//                System.out.println("onCompleted");
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//                System.out.println("onError");
+//            }
+//
+//            @Override
+//            public void onNext(Integer integer) {
+//                System.out.println("onNext: Hello " + integer);
+//            }
+//        });
+//
+//        Observable.from(list).subscribe(new Action1<Integer>() {
+//            @Override
+//            public void call(Integer integer) {
+//                System.out.println("call: Hello " + integer);
+//            }
+//        });
+
+
         return helloService.helloServcie();
         //return restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
     }
+
+
 }
