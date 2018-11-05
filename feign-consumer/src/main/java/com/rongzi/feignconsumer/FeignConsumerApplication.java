2309ee29318c5fc138bbc8ec9e531741bc3be898
houdongdong <houdongdong@rongzi.com>
2018-11-05 16:50:47 +0800

    添加hystrix, 支持断路器指标监控，关键是有这个URL：http://localhost:9001/actuator/hystrix.stream

--- feign-consumer/src/main/java/com/rongzi/feignconsumer/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/com/rongzi/feignconsumer/FeignConsumerApplication.java
@@ -3,10 +3,12 @@ package com.rongzi.feignconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FeignConsumerApplication {
